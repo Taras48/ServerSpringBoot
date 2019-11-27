2d71bfed8a59cd7ec93104804b819d54a832bc2d
Taras48 <red.shuhov48@gmail.com>
2019-11-27 07:09:23 +0300

    norm

--- src/main/java/com/server/config/SecurityConfig.java
+++ src/main/java/com/server/config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
@@ -46,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin/**").permitAll()//.hasAuthority("admin")//исправить
+                .antMatchers("/admin/**").hasAuthority("admin")//исправить
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
